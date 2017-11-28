adb0dce73195695c92be99da7c0fdb37be5c80b0
Leonid <leonid.sobolev285@gmail.com>
2017-11-28 16:59:23 +0300

    Nothing

---
+++ questions/model_managers.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class QuestionManager(models.Manager):
+    def with_rating(self):
+        questions = list(self.model.objects.all().order_by("-date"))
+        for question in questions:
+            from django.db import connection
+            cursor = connection.cursor()
+            cursor.execute("SELECT * from questions_questionlike as ql WHERE questionLiked_id = %s",
+                           (question.id,))
+            rating = 0
+            for row in cursor.fetchall():
+                if row[3]:
+                    rating += 1
+                else:
+                    rating -= 1
+            question.rating = rating
+        return questions
